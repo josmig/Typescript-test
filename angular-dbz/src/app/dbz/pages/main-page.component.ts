d19d461daf110a020d2738b5ff17b1976639216d
josmig <miguepantigoso@gmail.com>
2024-04-23 18:07:52 -0500

    uso del @inpu,ngfor y ngClass

--- angular-dbz/src/app/dbz/pages/main-page.component.ts
+++ angular-dbz/src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent {
+
+
+  public characters: Character[] = [
+    {name: ' Krilin' , power:10},
+    {name:'Goku', power:12000},
+    {name:'Vegeta', power:10000}
+  ];
+
+
 
 }
